8f410e529bd2c54dbd5939ea4b4631ac879ef454
David Alvarez <davidnuevoalvarez@gmail.com>
2025-07-18 03:28:27 +0000

    pues sigue igual

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -7,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC4Nmc7fs5XRFj0lE4LKhEzBagGr6Ttnyc",
   authDomain: "recaudo-seguro.firebaseapp.com",
   projectId: "recaudo-seguro",
-  storageBucket: "recaudo-seguro.firebasestorage.app",
+  storageBucket: "recaudo-seguro.appspot.com",
   messagingSenderId: "547595522209",
   appId: "1:547595522209:web:57a4f2824bea39f53ba0e0"
 };
