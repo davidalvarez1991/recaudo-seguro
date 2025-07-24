d958598451171cda8abe48d92105f219dc911288
David Alvarez <davidnuevoalvarez@gmail.com>
2025-07-24 21:26:50 +0000

    ahora pasa esto

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,12 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC4Nmc7fs5XRFj0lE4LKhEzBagGr6Ttnyc",
+  apiKey: "AIzaSyB-m11OcvdJzSXwA2sL3Oa1j5s_9s_8_g4",
   authDomain: "recaudo-seguro.firebaseapp.com",
   projectId: "recaudo-seguro",
   storageBucket: "recaudo-seguro.appspot.com",
@@ -14,8 +14,14 @@ const firebaseConfig = {
   appId: "1:547595522209:web:57a4f2824bea39f53ba0e0"
 };
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase for SSR
+let app: FirebaseApp;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
 const db = getFirestore(app);
 const storage = getStorage(app);
 
