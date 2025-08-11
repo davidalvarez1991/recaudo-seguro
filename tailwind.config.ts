d12aafadb46965ddc4687a43d7a0a070c2ea262e
David Alvarez <davidnuevoalvarez@gmail.com>
2025-08-11 15:27:01 +0000

    No se está ajustando sigue igua

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,9 +72,6 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      fontFamily: {
-        body: ["Inter", "sans-serif"],
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
